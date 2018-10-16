ed19100b6b6c65bf72038c26fd041a400a340d92
Ahmed Mohammed Maawy <ultimateprogramer@gmail.com>
2018-10-16 16:22:05 +0300

    Initializing the form rendering from Ushahidi with needed parameters

--- platform-brck-survey/src/App.js
+++ platform-brck-survey/src/App.js
@@ -18,13 +18,13 @@ class App extends Component {
   componentDidMount()
   {
     //read 
-    //api.getConfig().then( configData => this.setState({ config: configData }));
-    //api.getFormInfo(2).then( formInfo => this.setState({ formInfo: formInfo }));
-    //api.getFeatures().then( features => this.setState({ features }));
+    api.getConfig().then( configData => this.setState({ config: configData }));
+    api.getFormInfo(2).then( formInfo => this.setState({ formInfo: formInfo }));
+    api.getFeatures().then( features => this.setState({ features }));
     api.getStages().then( stages => this.setState({ stages }));
     api.getAttributes().then( attributes => this.setState({ attributes }));
-    //api.getTags().then( tags => this.setState({ tags }));
-    //api.getToken().then( headerToken => this.setState ( { headerToken } ))
+    api.getTags().then( tags => this.setState({ tags }));
+    api.getToken().then( headerToken => this.setState ( { headerToken } ))
   }
 
   render() {
--- platform-brck-survey/src/helpers/fetchData.js
+++ platform-brck-survey/src/helpers/fetchData.js
@@ -1,9 +1,9 @@
 import * as fetch from 'isomorphic-fetch';
 
 // @TODO: actually read these from the env
-const baseUrl = process.env.API_BASEURL || 'http://localhost:8000';
+const baseUrl = process.env.API_BASEURL || 'https://brck-tests.api.ushahidi.io';
 const apiPrefix = process.env.API_PREFIX || '/api/v3';
-const uiClientSecret = process.env.OAUTH_CLIENT_SECRET || '35e7f0bca957836d05ca0492211b0ac707671261';
+const uiClientSecret = process.env.OAUTH_CLIENT_SECRET || '3a845183-62f4-4d92-b1fc-29d81188b5aa';
 
 export const getConfig = () => fetch(`${baseUrl}${apiPrefix}/config`)
   .then((response) => {
